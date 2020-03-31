afe407a5d5e8e0cf2545a04ca3b0f351d259f632
Atul Sharma <atulsharma20may@gmail.com>
2020-03-31 13:39:51 +0530

    To upload files

---
+++ 02-Game variables.py
@@ -0,0 +1,11 @@
+import pygame
+pygame.init()
+
+
+# creating window
+gamewindow = pygame.display.set_mode((1200, 500))
+pygame.display.set_caption("my first game")
+
+# game specific variables
+exit_game = False
+game_over = False
