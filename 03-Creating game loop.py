e65568f57eaf62e1f853a3c9d85f36380952f636
Atul Sharma <atulsharma20may@gmail.com>
2020-03-31 13:39:55 +0530

    To upload files

---
+++ 03-Creating game loop.py
@@ -0,0 +1,17 @@
+import pygame
+pygame.init()
+
+# creating window
+gamewindow = pygame.display.set_mode((1200, 500))
+pygame.display.set_caption("my first game")
+
+# game specific variables
+exit_game = False
+game_over = False
+
+# creating a game loop
+while not exit_game:
+    pass
+pygame.quit()
+quit()
+
